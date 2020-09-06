390b528e6441683d3846627de6484a7494cf57b1
anirbmuk <anirbmuk@live.in>
2020-09-06 09:47:49 +0530

    Added sorting of output stream

--- src/app/autocomplete.service.ts
+++ src/app/autocomplete.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { BehaviorSubject, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AutocompleteService {
@@ -16,9 +16,15 @@ export class AutocompleteService {
   public action$ = this.actionSubject.asObservable();
 
   public autocomplete$ = this.action$.pipe(
-    switchMap(input => ((!!input && input.trim().length > 1) ?
-                           this.http.get<Region[]>(`${this.regionsUrl}\?city=^${input}`) :
-                           of([])))
+    // Taps the emitted value from action stream
+    tap(data => console.log('input:', data)),
+    // switchMap fires REST based on above input
+    switchMap(input => ((!!input && input.trim().length > 1) ? this.http.get<Region[]>(`${this.regionsUrl}\?city=^${input}`) : of([])).pipe(
+      // Additional sorting on switchMap output
+      map((regions: Region[]) => regions.sort((region1, region2) => region1.city.localeCompare(region2.city))),
+      // Taps the final emitted value from inner observable
+      tap(data => console.log('output:', data))
+    )),
   );
 
   public setAction(input: string): void {
