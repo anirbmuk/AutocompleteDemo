bce5ab0f1fbc11e595b5d10ee0c4bc15a71cdb5e
anirbmuk <anirbmuk@live.in>
2023-05-29 21:18:03 +0200

    split up code for easy readability

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -28,7 +28,7 @@ export class ProductService {
     // Taps the debounced emitted value from action stream
     tap(data => console.log('input:', data)),
     // switchMap fires REST based on above input
-    switchMap(input => ((input?.trim()?.length > 1) ? this.http.get<{ products: Product[] }>(`${this.productsUrl}/search?q=${input.toLowerCase()}`) : of({ products: [] }))
+    switchMap(input => ((input?.trim()?.length > 1) ? this.getDataFromEP(input) : of({ products: [] }))
       .pipe(
         // Additional sorting on switchMap output
         map(({ products }) => products.sort((product1, product2) => product1.title.localeCompare(product2.title))),
@@ -36,4 +36,9 @@ export class ProductService {
         tap(products => console.log('products:', products)),
       )),
   );
+
+  private getDataFromEP(input: string) {
+    const url = `${this.productsUrl}/search?q=${input.toLowerCase()}`;
+    return this.http.get<{ products: Product[] }>(url);
+  }
 }
