c4d42c8ff76269c3c594aa83739bd209c18ae1c6
anirbmuk <anirbmuk@live.in>
2023-05-29 20:09:53 +0200

    updated to standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { UntypedFormGroup, UntypedFormControl } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule, MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 import { ProductService } from './product.service';
 import { Product } from './product.interface';
 
+const CORE_MODULES = [CommonModule, ReactiveFormsModule] as const;
+const MATERIAL_MODULES = [MatFormFieldModule, MatInputModule, MatAutocompleteModule];
+
 @Component({
+  standalone: true,
+  imports: [...CORE_MODULES, ...MATERIAL_MODULES],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
@@ -12,8 +22,8 @@ import { Product } from './product.interface';
 })
 export class AppComponent {
 
-  readonly autocompleteForm = new UntypedFormGroup({
-    autocomplete: new UntypedFormControl('')
+  readonly autocompleteForm = new FormGroup<{ autocomplete: FormControl<string | null | undefined> }>({
+    autocomplete: new FormControl('')
   });
 
   constructor(private readonly productService: ProductService) { }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Subject, of } from 'rxjs';
@@ -9,7 +9,7 @@ import { Product } from './product.interface';
 @Injectable({ providedIn: 'root' })
 export class ProductService {
 
-  constructor(private readonly http: HttpClient) { }
+  private readonly http = inject(HttpClient);
 
   private productsUrl = 'https://dummyjson.com/products';
 
@@ -21,12 +21,12 @@ export class ProductService {
   }
 
   readonly products$ = this.action$.pipe(
-    // Taps the emitted value from action stream
-    tap(data => console.log('input:', data)),
     // Wait for 250 ms to allow the user to finish typing
     debounceTime(250),
     // Check if the input has changed from the last emitted value
     distinctUntilChanged(),
+    // Taps the debounced emitted value from action stream
+    tap(data => console.log('input:', data)),
     // switchMap fires REST based on above input
     switchMap(input => ((input?.trim()?.length > 1) ? this.http.get<{ products: Product[] }>(`${this.productsUrl}/search?q=${input.toLowerCase()}`) : of({ products: [] }))
       .pipe(
