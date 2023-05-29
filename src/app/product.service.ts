6155812a01ef9fa39747d2180226e7f2a58bc593
Anirban Mukherjee <31846607+anirbmuk@users.noreply.github.com>
2023-05-29 20:22:34 +0200

    Upgrade/upgrade to angular material 15 (#1)
    * removed bootstrap classes
    
    * Removed obsolete in-memory-web-api
    
    * updated to angular 14
    
    * updated to angular 15
    
    * updated to standalone components
    
    * chore: version updates
    
    * npm audit fix

---
+++ src/app/product.service.ts
@@ -0,0 +1,39 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Subject, of } from 'rxjs';
+import { switchMap, map, tap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
+import { Product } from './product.interface';
+
+@Injectable({ providedIn: 'root' })
+export class ProductService {
+
+  private readonly http = inject(HttpClient);
+
+  private productsUrl = 'https://dummyjson.com/products';
+
+  private actionSubject = new Subject<string | undefined | null>();
+  readonly action$ = this.actionSubject.asObservable();
+
+  setAction(input: string): void {
+    this.actionSubject.next(input);
+  }
+
+  readonly products$ = this.action$.pipe(
+    // Wait for 250 ms to allow the user to finish typing
+    debounceTime(250),
+    // Check if the input has changed from the last emitted value
+    distinctUntilChanged(),
+    // Taps the debounced emitted value from action stream
+    tap(data => console.log('input:', data)),
+    // switchMap fires REST based on above input
+    switchMap(input => ((input?.trim()?.length > 1) ? this.http.get<{ products: Product[] }>(`${this.productsUrl}/search?q=${input.toLowerCase()}`) : of({ products: [] }))
+      .pipe(
+        // Additional sorting on switchMap output
+        map(({ products }) => products.sort((product1, product2) => product1.title.localeCompare(product2.title))),
+        // Taps the final emitted value from mapping transformation
+        tap(products => console.log('products:', products)),
+      )),
+  );
+}
--- src/app/region.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface Region {
-  id: number;
-  state?: string;
-  capital?: string;
-}
